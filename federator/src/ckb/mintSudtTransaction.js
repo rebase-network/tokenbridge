0dafbe9aa5694316be6e46739d632945c447cef4
Harry Liu <2411mail@gmail.com>
2020-08-29 23:20:31 +0800

    format code

--- federator/src/ckb/mintSudtTransaction.js
+++ federator/src/ckb/mintSudtTransaction.js
@@ -1,10 +1,22 @@
 const _ = require('lodash')
 const BN = require('bn.js')
-const { addressToScript, ScriptHashType } = require('@keyper/specs')
-const { scriptToHash, toHexInLittleEndian } = require('@nervosnetwork/ckb-sdk-utils')
-const { getUnspentCells } = require('./utils/apis')
-const { getDepFromLockType }  = require('./utils/deps')
-const { getDepFromType }  = require('./utils/constants/typesInfo')
+const {
+	addressToScript,
+	ScriptHashType
+} = require('@keyper/specs')
+const {
+	scriptToHash,
+	toHexInLittleEndian
+} = require('@nervosnetwork/ckb-sdk-utils')
+const {
+	getUnspentCells
+} = require('./utils/apis')
+const {
+	getDepFromLockType
+} = require('./utils/deps')
+const {
+	getDepFromType
+} = require('./utils/constants/typesInfo')
 const getCKB = require('./utils/ckb')
 
 const SUDT_MIN_CELL_CAPACITY = 142;
@@ -25,7 +37,7 @@ const CKB_TOKEN_DECIMALS = 10 ** 8;
 //   total: any;
 // }
 
- const getInputCKBCells = async (lockHash, params) => {
+const getInputCKBCells = async (lockHash, params) => {
 	const unspentCells = await getUnspentCells(lockHash, params)
 	// Error handling
 	if (unspentCells.errCode !== undefined && unspentCells.errCode !== 0) {
@@ -47,7 +59,7 @@ const CKB_TOKEN_DECIMALS = 10 ** 8;
 	return inputCells
 }
 
- const getLockScriptName = (lockScript) => {
+const getLockScriptName = (lockScript) => {
 	// let lockScriptName = null
 	// if (lockScript.codeHash === ADDRESS_TYPE_CODEHASH.Secp256k1) {
 	// 	lockScriptName = 'Secp256k1'
@@ -60,9 +72,9 @@ const CKB_TOKEN_DECIMALS = 10 ** 8;
 }
 
 const TestDaiTypeScript = {
-    hashType: 'data',
-    codeHash: '0x48dbf59b4c7ee1547238021b4869bceedf4eea6b43772e5d66ef8865b6ae7212',
-    args: '0x0466a2e7b55dad9353271614ca3a1b6016d3c6b69e3239c6ba7e37ef1bbe0a0e',
+	hashType: 'data',
+	codeHash: '0x48dbf59b4c7ee1547238021b4869bceedf4eea6b43772e5d66ef8865b6ae7212',
+	args: '0x0466a2e7b55dad9353271614ca3a1b6016d3c6b69e3239c6ba7e37ef1bbe0a0e',
 };
 
 const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866ffd30cd287e88f8139dd0cad5deb9e189c68b4b26bf9899';
@@ -80,7 +92,7 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
  * @param {*} fee
  * @returns {CreateRawTxResult}
  */
- function mintSudtRawTx(
+function mintSudtRawTx(
 	inputCkbCells,
 	fromLockScript,
 	mintSudtAmount,
@@ -97,7 +109,10 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 		witnesses: [],
 		outputsData: [],
 	}
-	const config = { index: 0, length: -1 }
+	const config = {
+		index: 0,
+		length: -1
+	}
 
 	// 1.input CKB
 	for (let i = 0; i < inputCkbCells.cells.length; i++) {
@@ -115,8 +130,8 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 	}
 
 	// 1. output | mint sudt
-    const toLockHash = scriptToHash(toLockScript)
-    const fromLockHash = scriptToHash(fromLockScript)
+	const toLockHash = scriptToHash(toLockScript)
+	const fromLockHash = scriptToHash(fromLockScript)
 	const toSudtCapacity = SUDT_MIN_CELL_CAPACITY * CKB_TOKEN_DECIMALS
 	const toSudtOutputCell = {
 		capacity: `0x${new BN(toSudtCapacity).toString(16)}`,
@@ -131,7 +146,7 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 	const sUdtLeSend = toHexInLittleEndian(BigInt(mintSudtAmount), 16)
 	rawTx.outputsData.push(sUdtLeSend)
 
-    // 2. output | ckb charge
+	// 2. output | ckb charge
 	const ckbCharge =
 		BigInt(inputCkbCells.total) - BigInt(toSudtCapacity) - BigInt(fee)
 	rawTx.outputs.push({
@@ -148,11 +163,11 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 		target: scriptToHash(fromLockScript),
 		config,
 		tx: rawTx,
-    }
+	}
 	return signObj
 }
 
- const createSudtTransaction = async (
+const createSudtTransaction = async (
 	fromAddress,
 	toAddress,
 	sendSudtAmount,
@@ -167,15 +182,27 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 	// 1. input CKB cells
 	const inputCkbCells = await getInputCKBCells(fromLockHash, params)
 
-    const toLockScript = addressToScript(toAddress)
-    const fromLockScripeName = getLockScriptName(fromLockScript)
+	const toLockScript = addressToScript(toAddress)
+	const fromLockScripeName = getLockScriptName(fromLockScript)
 	const toLockScriptName = getLockScriptName(toLockScript)
 
-    const deps = [{"outPoint":{"txHash":"0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37","index":"0x0"},"depType":"depGroup"},{"outPoint":{"txHash":"0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958","index":"0x0"},"depType":"code"}];
+	const deps = [{
+		"outPoint": {
+			"txHash": "0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37",
+			"index": "0x0"
+		},
+		"depType": "depGroup"
+	}, {
+		"outPoint": {
+			"txHash": "0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958",
+			"index": "0x0"
+		},
+		"depType": "code"
+	}];
 
-    // const Secp256k1Dep =  [{"outPoint":{"txHash":"0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37","index":"0x0"},"depType":"depGroup"},{"outPoint":{"txHash":"0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958","index":"0x0"},"depType":"code"}];
-    // deps.push(Secp256k1Dep)
-    // if (fromLockScripeName === toLockScriptName) {
+	// const Secp256k1Dep =  [{"outPoint":{"txHash":"0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37","index":"0x0"},"depType":"depGroup"},{"outPoint":{"txHash":"0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958","index":"0x0"},"depType":"code"}];
+	// deps.push(Secp256k1Dep)
+	// if (fromLockScripeName === toLockScriptName) {
 	// 	const fromDepObj = await getDepFromLockType(fromLockScripeName)
 	// 	deps.push(Secp256k1Dep)
 	// } else {
@@ -183,7 +210,7 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 	// 	const toDepObj = await getDepFromLockType(toLockScriptName)
 	// 	deps.push(fromDepObj)
 	// 	deps.push(toDepObj)
-    // }
+	// }
 	// const sUdtDep = await getDepFromType('simpleudt')
 	// deps.push(sUdtDep)
 
@@ -207,14 +234,14 @@ const TestDaiTypeHash = scriptToHash(TestDaiTypeScript); // '0x7abd58773ffee5866
 //   return signedTx;
 // };
 
-async function mintSudtTransaction (
+async function mintSudtTransaction(
 	fromAddress,
 	toAddress,
 	mintSudtAmount,
 	fee,
 	privateKey
 ) {
-    const ckb = getCKB();
+	const ckb = getCKB();
 	const lockScript = addressToScript(fromAddress)
 	const lockHash = scriptToHash(lockScript)
 	const rawTxObj = await createSudtTransaction(
@@ -222,19 +249,19 @@ async function mintSudtTransaction (
 		toAddress,
 		mintSudtAmount,
 		fee
-    )
-    console.log(/rawTxObj/,JSON.stringify(rawTxObj));
+	)
+	console.log(/rawTxObj/, JSON.stringify(rawTxObj));
 
 	//   const signedTx = await signSudtTransaction(lockHash, password, rawTxObj);
 	const rawTx = rawTxObj.tx
 	const signedTx = await ckb.signTransaction(privateKey)(rawTx)
 	const txResultObj = {
 		txHash: null,
-    }
-    console.log(/signedTx/,JSON.stringify(signedTx));
+	}
+	console.log(/signedTx/, JSON.stringify(signedTx));
 	try {
-        const realTxHash = await ckb.rpc.sendTransaction(signedTx)
-        console.log(/realTxHash/,realTxHash);
+		const realTxHash = await ckb.rpc.sendTransaction(signedTx)
+		console.log(/realTxHash/, realTxHash);
 		txResultObj.txHash = realTxHash
 	} catch (error) {
 		console.error(`Failed to send tx: ${error}`)
